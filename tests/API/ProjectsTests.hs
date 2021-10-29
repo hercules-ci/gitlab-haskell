934651923f9ed296b68ba2ae238a98ca4f330d77
Rob Stewart <robstewart57@gmail.com>
2021-10-30 00:22:03 +0100

    Adds one Project JSON parser test with new testing API

--- tests/API/ProjectsTests.hs
+++ tests/API/ProjectsTests.hs
@@ -16,4 +16,39 @@ import Test.Tasty.HUnit
 -- | https://docs.gitlab.com/ee/api/projects.html
 projectsTests :: [TestTree]
 projectsTests =
-  []
+  concat $
+    [ let fname = "data/api/projects/list-all-projects.json"
+       in gitlabJsonParserTests
+            "list-all-projects"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Project])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Project]
+                parseOne (encode decodedFile) :: IO [Project]
+            )
+    ]
+
+{-
+
+Untested data files:
+
+archive-project.json
+get-single-project.json
+list-projects-starred-by-user.json
+unarchive-project.json
+get-path-to-repository-storage.json
+languages.json
+list-user-projects.json
+unstar-project.json
+get-project-hook.json
+star-project.json
+upload-file.json
+get-project-push-rules.json
+list-forks-of-project.json
+starrers-of-project.json
+upload-project-avatar.json
+get-project-users.json
+list-project-groups.json
+transfer-project-new-namespace.json
+
+-}
