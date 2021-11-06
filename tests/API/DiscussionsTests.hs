1fff8a346a0c32943a70444f197525939af137a8
Rob Stewart <robstewart57@gmail.com>
2021-11-06 23:16:24 +0000

    Adds discussion parsing tests

--- tests/API/DiscussionsTests.hs
+++ tests/API/DiscussionsTests.hs
@@ -16,4 +16,50 @@ import Test.Tasty.HUnit
 -- | https://docs.gitlab.com/ee/api/discussions.html
 discussionsTests :: [TestTree]
 discussionsTests =
-  []
+  concat $
+    [ let fname = "data/api/discussions/list-group-epic-discussion-items.json"
+       in gitlabJsonParserTests
+            "list-group-epic-discussion-items"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Discussion])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Discussion]
+                parseOne (encode decodedFile) :: IO [Discussion]
+            ),
+      let fname = "data/api/discussions/list-project-merge-request-discussion-items.json"
+       in gitlabJsonParserTests
+            "list-project-merge-request-discussion-items"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Discussion])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Discussion]
+                parseOne (encode decodedFile) :: IO [Discussion]
+            ),
+      let fname = "data/api/discussions/list-project-commit-discussion-items.json"
+       in gitlabJsonParserTests
+            "list-project-commit-discussion-items"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Discussion])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Discussion]
+                parseOne (encode decodedFile) :: IO [Discussion]
+            ),
+      let fname = "data/api/discussions/list-project-snippet-discussion-items.json"
+       in gitlabJsonParserTests
+            "list-project-snippet-discussion-items"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Discussion])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Discussion]
+                parseOne (encode decodedFile) :: IO [Discussion]
+            ),
+      let fname = "data/api/discussions/list-project-discussion-items.json"
+       in gitlabJsonParserTests
+            "list-project-discussion-items"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Discussion])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Discussion]
+                parseOne (encode decodedFile) :: IO [Discussion]
+            )
+    ]
