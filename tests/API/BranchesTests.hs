482fcd97d77c7f31fb1ad91640f832cef8b9459e
Rob Stewart <robstewart57@gmail.com>
2021-11-06 22:05:22 +0000

    Adds Branch parsing tests

--- tests/API/BranchesTests.hs
+++ tests/API/BranchesTests.hs
@@ -16,4 +16,32 @@ import Test.Tasty.HUnit
 -- | https://docs.gitlab.com/ee/api/branches.html
 branchesTests :: [TestTree]
 branchesTests =
-  []
+  concat $
+    [ let fname = "data/api/branches/list-repository-branches.json"
+       in gitlabJsonParserTests
+            "list-repository-branches"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Branch])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Branch]
+                parseOne (encode decodedFile) :: IO [Branch]
+            ),
+      let fname = "data/api/branches/get-single-repository-branch.json"
+       in gitlabJsonParserTests
+            "get-single-repository-branch"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Branch)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Branch
+                parseOne (encode decodedFile) :: IO Branch
+            ),
+      let fname = "data/api/branches/create-repository-branch.json"
+       in gitlabJsonParserTests
+            "create-repository-branch"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Branch)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Branch
+                parseOne (encode decodedFile) :: IO Branch
+            )
+    ]
