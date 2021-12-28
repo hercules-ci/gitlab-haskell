de1b55cd889b5f88d95ecfdb5205b82778520918
Rob Stewart <robstewart57@gmail.com>
2021-12-28 00:16:00 +0000

    Adds Tags parsing unit tests

--- tests/API/TagsTests.hs
+++ tests/API/TagsTests.hs
@@ -15,4 +15,32 @@ import Test.Tasty.HUnit
 -- | https://docs.gitlab.com/ee/api/tags.html
 tagsTests :: [TestTree]
 tagsTests =
-  []
+  concat
+    [ let fname = "data/api/tags/list-project-repository-tags.json"
+       in gitlabJsonParserTests
+            "list-project-repository-tags"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Tag])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Tag]
+                parseOne (encode decodedFile) :: IO [Tag]
+            ),
+      let fname = "data/api/tags/single-repository-tag.json"
+       in gitlabJsonParserTests
+            "single-repository-tag"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Tag)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Tag
+                parseOne (encode decodedFile) :: IO Tag
+            ),
+      let fname = "data/api/tags/create-new-tag.json"
+       in gitlabJsonParserTests
+            "create-new-tag"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Tag)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Tag
+                parseOne (encode decodedFile) :: IO Tag
+            )
+    ]
