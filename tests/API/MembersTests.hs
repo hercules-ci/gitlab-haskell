a8af658ef2baaea6c622faa159aa3ef5d5fec09e
Rob Stewart <robstewart57@gmail.com>
2022-01-29 17:52:58 +0000

    Adds Members parser tests

--- tests/API/MembersTests.hs
+++ tests/API/MembersTests.hs
@@ -15,4 +15,104 @@ import Test.Tasty.HUnit
 -- | https://docs.gitlab.com/ee/api/members.html
 membersTests :: [TestTree]
 membersTests =
-  []
+  concat
+    [ let fname = "data/api/members/list-pending-members.json"
+       in gitlabJsonParserTests
+            "list-project-repository-tags"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Member])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Member]
+                parseOne (encode decodedFile) :: IO [Member]
+            ),
+      let fname = "data/api/members/billable-members-of-group.json"
+       in gitlabJsonParserTests
+            "billable-members-of-group"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Member])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Member]
+                parseOne (encode decodedFile) :: IO [Member]
+            ),
+      let fname = "data/api/members/removed-override-flag-member-group.json"
+       in gitlabJsonParserTests
+            "removed-override-flag-member-group"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Member)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+                parseOne (encode decodedFile) :: IO Member
+            ),
+      let fname = "data/api/members/member-group-or-project-including-inherited-invited.json"
+       in gitlabJsonParserTests
+            "member-group-or-project-including-inherited-invited"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Member)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+                parseOne (encode decodedFile) :: IO Member
+            ),
+      let fname = "data/api/members/override-flag-member-group.json"
+       in gitlabJsonParserTests
+            "override-flag-member-group"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Member)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+                parseOne (encode decodedFile) :: IO Member
+            ),
+      let fname = "data/api/members/member-group-or-project.json"
+       in gitlabJsonParserTests
+            "member-group-or-project"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Member)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+                parseOne (encode decodedFile) :: IO Member
+            ),
+      let fname = "data/api/members/edit-member-group-project.json"
+       in gitlabJsonParserTests
+            "edit-member-group-project"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Member)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+                parseOne (encode decodedFile) :: IO Member
+            ),
+      let fname = "data/api/members/list-all-memembers-group-or-project-including-inherited-invited.json"
+       in gitlabJsonParserTests
+            "list-all-memembers-group-or-project-including-inherited-invited"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Member])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Member]
+                parseOne (encode decodedFile) :: IO [Member]
+            ),
+      let fname = "data/api/members/add-member-group-project.json"
+       in gitlabJsonParserTests
+            "add-member-group-project"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Member)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+                parseOne (encode decodedFile) :: IO Member
+            ),
+      let fname = "data/api/members/list-all-memembers-group-or-project.json"
+       in gitlabJsonParserTests
+            "list-all-memembers-group-or-project"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO [Member])
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO [Member]
+                parseOne (encode decodedFile) :: IO [Member]
+            )
+            -- let fname = "data/api/members/membership-billable-member-of-group.json"
+            --  in gitlabJsonParserTests
+            --       "membership-billable-member-of-group"
+            --       fname
+            --       (parseOne =<< BSL.readFile fname :: IO Member)
+            --       ( do
+            --           decodedFile <- parseOne =<< BSL.readFile fname :: IO Member
+            --           parseOne (encode decodedFile) :: IO Member
+            --       )
+    ]
