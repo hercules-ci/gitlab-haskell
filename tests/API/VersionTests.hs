5c00dc0e0eedc4b114d5e6c01ce12be1fe311a81
Rob Stewart <robstewart57@gmail.com>
2022-02-26 23:27:51 +0000

    Adds Version parser tests

--- tests/API/VersionTests.hs
+++ tests/API/VersionTests.hs
@@ -15,4 +15,14 @@ import Test.Tasty.HUnit
 -- | https://docs.gitlab.com/ee/api/version.html
 versionTests :: [TestTree]
 versionTests =
-  []
+  concat
+    [ let fname = "data/api/version/version.json"
+       in gitlabJsonParserTests
+            "version"
+            fname
+            (parseOne =<< BSL.readFile fname :: IO Version)
+            ( do
+                decodedFile <- parseOne =<< BSL.readFile fname :: IO Version
+                parseOne (encode decodedFile) :: IO Version
+            )
+    ]
