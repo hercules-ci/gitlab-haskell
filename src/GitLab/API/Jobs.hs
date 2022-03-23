7673d8a874da8d2cac8761a4102852739b85cadf
Rob Stewart <robstewart57@gmail.com>
2022-03-23 21:18:19 +0000

    Completes coverage of the REST API for Jobs

--- src/GitLab/API/Jobs.hs
+++ src/GitLab/API/Jobs.hs
@@ -25,7 +25,8 @@ jobs project = do
   result <- jobs' (project_id project)
   return (fromRight (error "jobs error") result)
 
--- | returns all jobs ran on a project given its project ID.
+-- | Get a list of jobs in a project. Jobs are sorted in descending
+-- order of their IDs.
 jobs' ::
   -- | the project ID
   Int ->
@@ -37,3 +38,121 @@ jobs' projectId =
       "/projects/"
         <> T.pack (show projectId)
         <> "/jobs"
+
+-- | Get a list of jobs for a pipeline.
+pipelineJobs ::
+  -- | the project
+  Project ->
+  -- | pipeline ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) [Job])
+pipelineJobs prj pipelineId =
+  gitlabGetMany addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipelines/"
+        <> T.pack (show pipelineId)
+        <> "/jobs"
+
+-- | Get a list of bridge jobs for a pipeline.
+pipelineBridges ::
+  -- | the project
+  Project ->
+  -- | pipeline ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) [Job])
+pipelineBridges prj pipelineId =
+  gitlabGetMany addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipelines/"
+        <> T.pack (show pipelineId)
+        <> "/bridges"
+
+-- | Get a single job of a project.
+job ::
+  -- | the project
+  Project ->
+  -- | job ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Job))
+job prj jobId =
+  gitlabGetOne addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/jobs/"
+        <> T.pack (show jobId)
+
+-- | Cancel a single job of a project.
+cancelJob ::
+  -- | the project
+  Project ->
+  -- | job ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Job))
+cancelJob prj jobId =
+  gitlabPost addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/jobs/"
+        <> T.pack (show jobId)
+        <> "/cancel"
+
+-- | Retry a single job of a project.
+retryJob ::
+  -- | the project
+  Project ->
+  -- | job ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Job))
+retryJob prj jobId =
+  gitlabPost addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/jobs/"
+        <> T.pack (show jobId)
+        <> "/cancel"
+
+-- | Retry a single job of a project.
+eraseJob ::
+  -- | the project
+  Project ->
+  -- | job ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Job))
+eraseJob prj jobId =
+  gitlabPost addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/jobs/"
+        <> T.pack (show jobId)
+        <> "/erase"
+
+-- | Triggers a manual action to start a job.
+runJob ::
+  -- | the project
+  Project ->
+  -- | job ID
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Job))
+runJob prj jobId =
+  gitlabPost addr []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/jobs/"
+        <> T.pack (show jobId)
+        <> "/play"
