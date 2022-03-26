c6989e0d12742799058fca1e965198ad02fba5f5
Rob Stewart <robstewart57@gmail.com>
2022-03-26 21:32:35 +0000

    Completes coverage of the REST API for Pipelines

--- src/GitLab/API/Pipelines.hs
+++ src/GitLab/API/Pipelines.hs
@@ -11,12 +11,15 @@ module GitLab.API.Pipelines where
 
 import qualified Data.ByteString.Lazy as BSL
 import Data.Either
+import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import GitLab.Types
 import GitLab.WebRequests.GitLabWebCalls
 import Network.HTTP.Client
 
--- | returns the pipelines for a project.
+-- | List pipelines in a project. Child pipelines are not included in
+-- the results, but you can get child pipeline individually.
 pipelines ::
   -- | the project
   Project ->
@@ -40,33 +43,113 @@ pipelines' projectId =
         <> T.pack (show projectId)
         <> "/pipelines"
 
--- | get a pipeline’s test report.  Since GitLab 13.0.
+-- | Get one pipeline from a project.
+pipeline ::
+  -- | the project
+  Project ->
+  -- | 	The ID of a pipeline
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Pipeline))
+pipeline prj pipelineId =
+  gitlabGetOne
+    addr
+    []
+  where
+    addr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipelines/"
+        <> T.pack (show pipelineId)
+
+-- | get a pipeline’s test report. Since GitLab 13.0.
 pipelineTestReport ::
   -- | the project
   Project ->
-  -- | the pipeline
-  Pipeline ->
-  GitLab TestReport
-pipelineTestReport proj pipeline = do
-  result <- pipelineTestReport' (project_id proj) (pipeline_id pipeline)
-  case fromRight (error "pipelineTestReport error") result of
-    Nothing -> error "pipelineTestReport error"
-    Just testReport -> return testReport
-
--- | get a pipeline’s test report. Since GitLab 13.0.
-pipelineTestReport' ::
-  -- | the project ID
-  Int ->
   -- | the pipeline ID
   Int ->
   GitLab (Either (Response BSL.ByteString) (Maybe TestReport))
-pipelineTestReport' projId pipelineId = do
+pipelineTestReport prj pipelineId = do
   let urlPath =
         T.pack
           ( "/projects/"
-              <> show projId
+              <> show (project_id prj)
               <> "/pipelines/"
               <> show pipelineId
               <> "/test_report"
           )
   gitlabGetOne urlPath []
+
+-- | Create a new pipeline. Since GitLab 14.6.
+newPipeline ::
+  -- | the project
+  Project ->
+  -- | The branch or tag to run the pipeline on.
+  Text ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Pipeline))
+newPipeline prj ref = do
+  gitlabPost
+    pipelineAddr
+    [("ref", Just (T.encodeUtf8 ref))]
+  where
+    pipelineAddr :: Text
+    pipelineAddr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipeline"
+
+-- | Retry a pipeline. Since GitLab 14.6.
+retryPipeline ::
+  -- | the project
+  Project ->
+  -- | The ID of a pipeline
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Pipeline))
+retryPipeline prj pipelineId = do
+  gitlabPost
+    pipelineAddr
+    []
+  where
+    pipelineAddr :: Text
+    pipelineAddr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipelines/"
+        <> T.pack (show pipelineId)
+        <> "/retry"
+
+-- | Cancel a pipeline's jobs.
+cancelPipelineJobs ::
+  -- | the project
+  Project ->
+  -- | The ID of a pipeline
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Pipeline))
+cancelPipelineJobs prj pipelineId = do
+  gitlabPost
+    pipelineAddr
+    []
+  where
+    pipelineAddr :: Text
+    pipelineAddr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipelines/"
+        <> T.pack (show pipelineId)
+        <> "/cancel"
+
+-- | Delete a pipline. Since GitLab 14.6.
+deletePipeline ::
+  -- | the project
+  Project ->
+  -- | The ID of a pipeline
+  Int ->
+  GitLab (Either (Response BSL.ByteString) (Maybe ()))
+deletePipeline prj pipelineId = do
+  gitlabDelete pipelineAddr
+  where
+    pipelineAddr :: Text
+    pipelineAddr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/pipelines/"
+        <> T.pack (show pipelineId)
