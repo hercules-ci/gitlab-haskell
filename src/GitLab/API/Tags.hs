c5641fd430ea1a2683f4812d1dc593ca969b44f1
Rob Stewart <robstewart57@gmail.com>
2022-03-28 22:16:09 +0100

    Completes coverage of the REST API for Tags

--- src/GitLab/API/Tags.hs
+++ src/GitLab/API/Tags.hs
@@ -3,37 +3,87 @@
 -- |
 -- Module      : Tags
 -- Description : Queries about tags in repositories
--- Copyright   : (c) Jihyun Yu, 2021
+-- Copyright   : (c) Jihyun Yu, 2021; Rob Stewart, 2022
 -- License     : BSD3
--- Maintainer  : yjh0502@gmail.com
+-- Maintainer  : yjh0502@gmail.com, robstewart57@gmail.com
 -- Stability   : stable
 module GitLab.API.Tags where
 
 import qualified Data.ByteString.Lazy as BSL
-import Data.Either
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import GitLab.Types
 import GitLab.WebRequests.GitLabWebCalls
 import Network.HTTP.Client
 
--- | returns all commits with tags.
+-- | Get a list of repository tags from a project.
 tags ::
-  -- | project
+  -- | the project
   Project ->
-  GitLab [Tag]
-tags project = do
-  result <- tags' (project_id project)
-  return (fromRight [] result)
-
--- | returns all commits with tags from a project given its project ID.
-tags' ::
-  -- | project ID
-  Int ->
   GitLab (Either (Response BSL.ByteString) [Tag])
-tags' projectId = do
-  gitlabGetMany (commitsAddr projectId) []
+tags prj = do
+  gitlabGetMany (commitsAddr (project_id prj)) []
   where
     commitsAddr :: Int -> Text
     commitsAddr projId =
-      "/projects/" <> T.pack (show projId) <> "/repository" <> "/tags"
+      "/projects/"
+        <> T.pack (show projId)
+        <> "/repository"
+        <> "/tags"
+
+-- | Get a specific repository tag determined by its name.
+tag ::
+  -- | the project
+  Project ->
+  -- | the name of the tag
+  Text ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Tag))
+tag prj tagName = do
+  gitlabGetOne (commitsAddr (project_id prj)) []
+  where
+    commitsAddr :: Int -> Text
+    commitsAddr projId =
+      "/projects/"
+        <> T.pack (show projId)
+        <> "/repository"
+        <> "/tags/"
+        <> tagName
+
+-- | Creates a new tag in the repository that points to the supplied
+-- ref.
+createTag ::
+  -- | the project
+  Project ->
+  -- | the name of the tag
+  Text ->
+  -- | Create tag using commit SHA, another tag name, or branch name
+  Text ->
+  GitLab (Either (Response BSL.ByteString) (Maybe Tag))
+createTag prj tagName ref = do
+  gitlabPost newTagAddr [("tag_name", Just (T.encodeUtf8 tagName)), ("ref", Just (T.encodeUtf8 ref))]
+  where
+    newTagAddr :: Text
+    newTagAddr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/repository"
+        <> "/tags"
+
+-- | Deletes a tag of a repository with given name.
+deleteTag ::
+  -- | the project
+  Project ->
+  -- | the name of the tag
+  Text ->
+  GitLab (Either (Response BSL.ByteString) (Maybe ()))
+deleteTag prj tagName =
+  gitlabDelete tagAddr
+  where
+    tagAddr :: Text
+    tagAddr =
+      "/projects/"
+        <> T.pack (show (project_id prj))
+        <> "/repository"
+        <> "/tags/"
+        <> tagName
